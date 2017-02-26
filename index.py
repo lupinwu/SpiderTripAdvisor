f152b7f067391032ef32bc3d25d1248116da6e62
lupinwu <mailwp@163.com>
2017-02-26 23:30:39 +0800

    add titles, imgs

--- index.py
+++ index.py
@@ -1,7 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://github.com/mugglecoding/Plan-for-combating.git'
+url = 'http://www.tripadvisor.cn/Attractions-g60763-Activities-c47-New_York_City_New_York.html'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
-print(soup)
+titles = soup.select('div.property_title > a')
+imgs = soup.select('img[width="160"]')
+print(titles, imgs)
+
