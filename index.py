2e6db8e0458095019a8b3e2aefaa58840287a319
lupinwu <mailwp@163.com>
2017-03-14 20:16:04 +0800

    delete imgs

--- index.py
+++ index.py
@@ -13,12 +13,10 @@ def get_attractions(url, data=None):
     time.sleep(2)  # 每隔2秒请求一次,防止反爬虫
     soup = BeautifulSoup(response.text, 'lxml')
     titles = soup.select('div.property_title > a[target="_blank"]')
-    imgs = soup.select('img[width="160"]')   # 图片爬取,由于js为#lazyload无法实现
     cates = soup.select('div.p13n_reasoning_v2')
-    for title, img, cate in zip(titles, imgs, cates):
+    for title, cate in zip(titles, cates):
         data = {
             'title': title.get_text(),
-            'img': img.get('src'),
             'cate': list(cate.stripped_strings),
         }
         print(data)
